5fdbe3a4407ef6b9fed76564982904fbc3c3543f
Tanmay Vakare <tanmayvakare@gmail.com>
2018-06-08 19:32:31 +0530

    Update load_job.py

--- spark_jobs/load_job.py
+++ spark_jobs/load_job.py
@@ -1,10 +1,11 @@
+"""This is a pyspark job to fetch all case images from mongoDB and stores the image features along with the object_id in a parqueet file on local storage. This makes processing faster as image feature dectection and extraction is a tedious process. The program uses SIFT to detect and extract features from images. This job can be scheduled to run at regular intervals(daily) to provide updated dataset for search jods. Refer README.md for more details."""
+
 import cv2
 import numpy as np
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
-#from pyspark import SaveMode
 
-#initialising SparkContext
+#initializing SparkContext
 spark_c = SparkContext()
 
 #function to extract features from images
@@ -24,7 +25,7 @@ def extract_feature(case_data):
 spark_s = SparkSession\
 .builder\
 .appName("Get Data from DB")\
-.config("spark.mongodb.input.uri","mongodb://userdata:userdata@medsearchcluster-shard-00-00-5lyma.mongodb.net:27017,medsearchcluster-shard-00-01-5lyma.mongodb.net:27017,medsearchcluster-shard-00-02-5lyma.mongodb.net:27017/test_db.cases?ssl=true&replicaSet=MedSearchCluster-shard-0&authSource=admin")\
+.config("spark.mongodb.input.uri","mongodb://<Username>:<Password>@<MongoDB url to collection with cases>")\
 .getOrCreate()
 
 #Loading or importing data from MongoDB cluster
